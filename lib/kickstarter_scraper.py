0d914d5550f788f912e65161dd710e68f6ab438f
michael youssef <michaelyoussef396@gmail.com>
2024-07-31 20:29:47 +1000

    completed follow along

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -6,7 +6,7 @@ import ipdb
 # image link: project.select("div.project-thumbnail a img")[0]['src']
 # description: project.select("p.bbcard_blurb")[0].text
 # location: project.select("ul.project-meta span.location-name")[0].text
-# percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%", "")
+# percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
 
 def create_project_dict():
     html = ''
@@ -15,7 +15,7 @@ def create_project_dict():
 
     kickstarter = BeautifulSoup(html, 'html.parser')
     projects = {}
-
+    
     # Iterate through the projects
     for project in kickstarter.select("li.project.grid_4"):
         title = project.select("h2.bbcard_name strong a")[0].text
@@ -29,13 +29,6 @@ def create_project_dict():
     # return the projects dictionary
     return projects
 
-# To run and see the results
-if __name__ == "__main__":
-    projects = create_project_dict()
-    for title, details in projects.items():
-        print(f"Title: {title}")
-        print(f"Image Link: {details['image_link']}")
-        print(f"Description: {details['description']}")
-        print(f"Location: {details['location']}")
-        print(f"Percent Funded: {details['percent_funded']}")
-        print("-" * 20)
+# Call the function and print the result
+projects = create_project_dict()
+print(projects)
